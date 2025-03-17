0bf1299e1dd771ab7135fc5b9776016ad7c0e10f
calint <calin.tenitchi@gmail.com>
2025-03-17 21:16:19 +0100

    .

--- src/startup.s
+++ src/startup.s
@@ -1,4 +1,13 @@
 .global _start
 _start:
+    la a0, __bss_start__
+    la a1, __bss_end__
+    li a2, 0
+clear_loop:
+    bge a0, a1, clear_done
+    sb a2, (a0)
+    addi a0, a0, 1
+    j clear_loop
+clear_done:
     li sp, 0x200000
     j run

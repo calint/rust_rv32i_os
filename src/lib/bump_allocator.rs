b7c6897f10f6c2dff6734553627131e0483ebe76
calint <calin.tenitchi@gmail.com>
2025-03-23 13:08:45 +0100

    .

--- src/lib/bump_allocator.rs
+++ src/lib/bump_allocator.rs
@@ -1,30 +1,22 @@
 use core::alloc::{GlobalAlloc, Layout};
-use core::cell::UnsafeCell;
 
 struct BumpAllocator {
-    next: UnsafeCell<usize>,
+    next: usize,
 }
-
-unsafe impl Sync for BumpAllocator {}
 
 unsafe impl GlobalAlloc for BumpAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let size = layout.size();
         let align = layout.align();
-        let next = self.next.get();
+        let aligned_next = (self.next + align - 1) & !(align - 1);
         unsafe {
-            let aligned_next = (*next + align - 1) & !(align - 1);
-            let new_next = aligned_next + size;
-            *next = new_next;
-            let ptr = aligned_next as *mut u8;
-            ptr
+            (self as *const Self as *mut Self).as_mut().unwrap().next = aligned_next + size;
         }
+        aligned_next as *mut u8
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
 }
 
 #[global_allocator]
-static ALLOCATOR: BumpAllocator = BumpAllocator {
-    next: UnsafeCell::new(0x1_0000),
-};
+static mut ALLOCATOR: BumpAllocator = BumpAllocator { next: 0x1_0000 };

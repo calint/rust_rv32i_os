e961cda469586331dd7a171d52e8539f43c6f1c0
calint <calin.tenitchi@gmail.com>
2025-03-29 18:33:55 +0700

    .

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -e
 cd $(dirname "$0")
 
+touch firmware.img.bak
+cp -a firmware.img firmware.img.bak || true
 scripts/firmware-build.sh
 
 # run
@@ -11,6 +13,14 @@ emulator/qa/test.sh
 
 # run tests
 qa/emulator/test.sh
+
+old_size=$(stat -c%s "firmware.img.bak")
+new_size=$(stat -c%s "firmware.img")
+size_diff=$((new_size - old_size))
+
+echo " * stats"
+echo "binary size change: $size_diff bytes"
+rm firmware.img.bak
 
 echo " * run emulator"
 emulator/osqa firmware.img sdcard.img

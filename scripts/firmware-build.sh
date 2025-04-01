3aa714e30ac6d522907f1f7426159b1490c8a275
calint <calin.tenitchi@gmail.com>
2025-04-01 12:43:57 +0700

    .

--- scripts/firmware-build.sh
+++ scripts/firmware-build.sh
@@ -14,6 +14,16 @@ cargo clippy --release -- -W clippy::all -W clippy::pedantic -W clippy::nursery 
                           -W clippy::style -W clippy::suspicious \
                           -W clippy::unwrap_used -W clippy::expect_used
 
+#                          -W clippy::restriction \
+#                          -W clippy::unwrap_used -W clippy::expect_used \
+#                          -A clippy::single_call_fn -A clippy::indexing_slicing -A clippy::missing_docs_in_private_items \
+#                          -A clippy::implicit_return -A clippy::unseparated_literal_suffix -A clippy::single_char_lifetime_names \
+#                          -A clippy::min_ident_chars -A clippy::arithmetic_side_effects -A clippy::default_numeric_fallback \
+#                          -A clippy::as_conversions -A clippy::panic -A clippy::multiple_unsafe_ops_per_block \
+#                          -A clippy::undocumented_unsafe_blocks -A clippy::arbitrary_source_item_ordering \
+#                          -A clippy::missing_trait_methods -A clippy::module_name_repetitions -A clippy::missing_assert_message \
+
+
 cargo build --release
 $OBJCOPY -O binary $ELF $FIRMWARE.img
 #$OBJDUMP --source-comment -Mnumeric,no-aliases -Sr $ELF > firmware.lst

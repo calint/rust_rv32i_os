47c645323407e9492cc0c430cda03b1b187d1805
calint <calin.tenitchi@gmail.com>
2025-04-14 16:26:28 +0700

    .

--- src/startup.s
+++ src/startup.s
@@ -4,12 +4,12 @@ _start:
     la a0, __bss_start__
     la a1, __bss_end__
     li a2, 0
-bss_clear_loop:
-    bge a0, a1, bss_clear_done
+.bss_clear_loop:
+    bge a0, a1, .bss_clear_done
     sb a2, (a0)
     addi a0, a0, 1
-    j bss_clear_loop
-bss_clear_done:
+    j .bss_clear_loop
+.bss_clear_done:
     # set stack pointer and enter program
     li sp, 0x200000
     j run

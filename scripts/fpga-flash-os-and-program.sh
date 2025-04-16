215a1f669d885bbf88b973bc204fb1bbbae1eef2
calint <calin.tenitchi@gmail.com>
2025-04-16 09:26:43 +0700

    .

--- scripts/fpga-flash-os-and-program.sh
+++ scripts/fpga-flash-os-and-program.sh
@@ -4,6 +4,7 @@ cd $(dirname "$0")
 
 cd ..
 scripts/firmware-build.sh
-openFPGALoader --external-flash firmware.img
+openFPGALoader --detect
+openFPGALoader --external-flash --board tangnano9k firmware.img
 openFPGALoader ../tang-nano-9k--riscv--cache-psram/impl/pnr/riscv.fs
 #openFPGALoader --write-flash ../tang-nano-9k--riscv--cache-psram/impl/pnr/riscv.fs

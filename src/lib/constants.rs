d33f02a7a38d71d37a99ca3f23fc4b039d20c01b
calint <calin.tenitchi@gmail.com>
2025-04-21 17:50:33 +0700

    .

--- src/lib/constants.rs
+++ src/lib/constants.rs
@@ -1,12 +1,12 @@
 //
 // reviewed: 2025-04-21
 //
-pub const MEMORY_END: u32 = 0x0020_0000;
-pub const LED: u32 = 0xffff_fffc;
-pub const UART_OUT_ADDR: u32 = 0xffff_fff8;
-pub const UART_IN_ADDR: u32 = 0xffff_fff4;
-pub const SDCARD_BUSY: u32 = 0xffff_fff0;
-pub const SDCARD_READ_SECTOR: u32 = 0xffff_ffec;
-pub const SDCARD_NEXT_BYTE: u32 = 0xffff_ffe8;
-pub const SDCARD_STATUS: u32 = 0xffff_ffe4;
-pub const SDCARD_WRITE_SECTOR: u32 = 0xffff_ffe0;
+pub(super) const MEMORY_END: u32 = 0x0020_0000;
+pub(super) const LED: u32 = 0xffff_fffc;
+pub(super) const UART_OUT_ADDR: u32 = 0xffff_fff8;
+pub(super) const UART_IN_ADDR: u32 = 0xffff_fff4;
+pub(super) const SDCARD_BUSY: u32 = 0xffff_fff0;
+pub(super) const SDCARD_READ_SECTOR: u32 = 0xffff_ffec;
+pub(super) const SDCARD_NEXT_BYTE: u32 = 0xffff_ffe8;
+pub(super) const SDCARD_STATUS: u32 = 0xffff_ffe4;
+pub(super) const SDCARD_WRITE_SECTOR: u32 = 0xffff_ffe0;

f3e6d72e33873818fc18b2267abf5f6693f661f4
calint <calin.tenitchi@gmail.com>
2025-04-26 09:44:25 +0700

    .

--- notes/README.md
+++ notes/README.md
@@ -2,7 +2,20 @@
 `script -I log -c scripts/emulator-run.sh`
 
 ## full clippy
-`cargo clippy --fix --release -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::correctness -W clippy::perf -W clippy::style -W clippy::suspicious -W clippy::unwrap_used -W clippy::unseparated_literal_suffix`
+```
+cargo clippy --fix --release -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::correctness -W clippy::perf -W clippy::style -W clippy::suspicious -W clippy::unwrap_used -W clippy::unseparated_literal_suffix
+
+
+#                          -W clippy::nursery -W clippy::restriction \
+#                          -W clippy::unwrap_used -W clippy::expect_used \
+#                          -A clippy::single_call_fn -A clippy::indexing_slicing -A clippy::missing_docs_in_private_items \
+#                          -A clippy::implicit_return -A clippy::single_char_lifetime_names \
+#                          -A clippy::min_ident_chars -A clippy::arithmetic_side_effects -A clippy::default_numeric_fallback \
+#                          -A clippy::as_conversions -A clippy::panic -A clippy::multiple_unsafe_ops_per_block \
+#                          -A clippy::undocumented_unsafe_blocks -A clippy::arbitrary_source_item_ordering \
+```
+
+
 
 ## escaped characters and \0
 Use `cutecom` and send files to validate FPGA function.
--- scripts/firmware-build.sh
+++ scripts/firmware-build.sh
@@ -16,17 +16,9 @@ cd ..
 
 cargo clean
 cargo clippy --release -- \
-  -W clippy::all -W clippy::pedantic -W clippy::correctness -W clippy::perf -W clippy::style -W clippy::suspicious \
+  -W clippy::all -W clippy::pedantic -W clippy::correctness -W clippy::perf \
+  -W clippy::style -W clippy::suspicious \
   -W clippy::unwrap_used -W clippy::unseparated_literal_suffix
-
-#                          -W clippy::nursery -W clippy::restriction \
-#                          -W clippy::unwrap_used -W clippy::expect_used \
-#                          -A clippy::single_call_fn -A clippy::indexing_slicing -A clippy::missing_docs_in_private_items \
-#                          -A clippy::implicit_return -A clippy::single_char_lifetime_names \
-#                          -A clippy::min_ident_chars -A clippy::arithmetic_side_effects -A clippy::default_numeric_fallback \
-#                          -A clippy::as_conversions -A clippy::panic -A clippy::multiple_unsafe_ops_per_block \
-#                          -A clippy::undocumented_unsafe_blocks -A clippy::arbitrary_source_item_ordering \
-#                          -A clippy::missing_trait_methods -A clippy::module_name_repetitions -A clippy::missing_assert_message \
 
 cargo build --release
 
@@ -42,7 +34,10 @@ old_file_size=$(stat -c "%s" "$FIRMWARE_IMG")
 
 $OBJCOPY -O binary "$ELF" "$FIRMWARE_IMG"
 $OBJDUMP --source-comment -SCr "$ELF" > "$FIRMWARE_LIST"
-$OBJDUMP -s --section=.rodata --section=.srodata --section=.data --section=.sdata --section=.bss --section=.sbss "$ELF" > "$FIRMWARE_DATA" || true
+$OBJDUMP -s --section=.rodata --section=.srodata \
+            --section=.data --section=.sdata \
+            --section=.bss --section=.sbss \
+            "$ELF" > "$FIRMWARE_DATA" || true
 
 ls -l --color "$FIRMWARE_IMG"
 

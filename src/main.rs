189a29a010126099005183b3a46165ca6b96da26
calint <calin.tenitchi@gmail.com>
2026-08-21 17:06:59 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ static HELP: &[u8] = b"command:\r
 \x20 mi: memory allocation info\r
 \x20 wait: do nothing\r
 \x20 help: this message\r
-";
+\r";
 
 static CREATION: &[u8] = b"sln todo: find an exit
 nl none back office

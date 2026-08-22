7929db4abacb7272fb4f52b64b41c3a2cbcf58ab
calint <calin.tenitchi@gmail.com>
2026-08-22 09:41:51 +0200

    .

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 cd $(dirname "$0")
 
 touch firmware.img.bak
-cp -a firmware.img firmware.img.bak || true
+cp --archive firmware.img firmware.img.bak || true
 
 scripts/firmware-build.sh
 
@@ -11,8 +11,8 @@ echo " * compare previous firmware.img with current"
 if cmp --silent firmware.img firmware.img.bak; then
     echo "no change"
 else
-    old_size=$(stat -c%s firmware.img.bak)
-    new_size=$(stat -c%s firmware.img)
+    old_size=$(stat --format=%s firmware.img.bak)
+    new_size=$(stat --format=%s firmware.img)
     size_diff=$((new_size - old_size))
     echo "changed, new image size difference: $size_diff bytes"
 fi
